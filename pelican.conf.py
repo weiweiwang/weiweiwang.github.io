2e613e736a6aab4a9bc46d79b52482d02fbb23b8
wangweiwei <wangweiwei@dianxinos.me>
2012-06-11 10:56:32 +0800

    init myblog

---
+++ pelican.conf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"wangweiwei"
+SITENAME = u"Cloud or not"
+SITEURL = '/'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG='zh'
+
+STATIC_PATHS=['./images',]
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DEFAULT_PAGINATION = 20
+
+
+    
---
+++ src/redis.md
@@ -0,0 +1,5 @@
+title: get familar with redis:a great structure server
+slug: get-familar-with-redis
+date: 2012-6-11
+category: nosql
+tags: redis,nosql,memcache
